dc43e3eefa0ec8d316fef6b8cba82d06a13ac193
dave1415 <dawitgetaneh99@gmail.com>
2022-10-11 14:49:30 -0700

    dave

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,39 +1,34 @@
 #include "sort.h"
-/**
- * swap_bubble - function to swap/sort.
- *@a: to be swapped.
- *@b: to be swapped.
- * Return: Always 0
- */
-void swap_bubble(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
 
 /**
- * shell_sort - sorts array with shell sort algorithm.
- *@array: array to be sorted.
- *@size: size of array.
- * Return: Always 0
- */
+* shell_sort - Sorts array using Knuth Sequence of Shell sort
+* @array: Array to be sorted
+* @size: Size of array to sort
+*/
+
 void shell_sort(int *array, size_t size)
 {
-	size_t gap;
-	size_t i, j;
+	size_t i, gap = 1, k;
+	int v;
 
-	for (gap = 1; gap < (size / 3);)
-		gap *= 4;
-	for (; gap >= 1; gap = gap / 3)
+	if (array == NULL)
+		return;
+	while (gap < size)
+		gap = 3 * gap + 1;
+	if (gap >= size)
+		gap = (gap - 1) / 3;
+	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
-			for (j = i; j >= gap && array[j - gap] > array[j]; j -= gap)
-				swap_bubble(array + j, array + (j - gap));
+			v = array[i];
+			for (k = i; (k > gap - 1) && (v < array[k - gap]); k -= gap)
+			{
+				array[k] = array[k - gap];
+			}
+			array[k] = v;
 		}
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
